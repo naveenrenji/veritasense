89d757a2c56d11f5f0bc52af5c3b5ab5a9fdd2a1
Naveen Mathews <naveenrenji55@gmail.com>
2024-02-27 23:13:34 -0500

    new trial

--- Backend/stageThree/responseGenerator.py
+++ Backend/stageThree/responseGenerator.py
@@ -1,37 +1,10 @@
-from transformers import PegasusForConditionalGeneration
-from transformers import PegasusTokenizer
+# Use a pipeline as a high-level helper
 from transformers import pipeline
 
 
 def response_generator(question, context):
-    input_text = f"Question: {question} Context: {context}"
-    # Pick model
-    model_name = "google/pegasus-xsum"
+    pipe = pipeline("question-answering", model="google/gemma-2b")
+    response = pipe(question=question, context=context)
+    return response['answer']
 
-    # Load pretrained tokenizer
-    pegasus_tokenizer = PegasusTokenizer.from_pretrained(model_name)
-
-    # Define PEGASUS model
-    pegasus_model = PegasusForConditionalGeneration.from_pretrained(model_name)
-
-    # Create tokens
-    tokens = pegasus_tokenizer(input_text, truncation=True, padding="longest", return_tensors="pt")
-    
-    # Summarize text
-    #encoded_summary = pegasus_model.generate(**tokens)
-
-    # Define summarization pipeline 
-    summarizer = pipeline(
-        "summarization", 
-        model=model_name, 
-        tokenizer="google/pegasus-xsum", 
-        framework="pt"
-    )
-
-    # Create summary 
-    summary = summarizer(input_text, min_length=30, max_length=150)
-    response = summary[0]["summary_text"]
-
-    return response
-
-# print(answer_question(question, context))
+res = print(response_generator("tell me about myself", "My name is Naveen I am a boy forom bahrain and i love to play agmes with all my fiendds"))
