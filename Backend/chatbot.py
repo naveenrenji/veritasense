92a3ef62ff4f8f2e48b2cf677bd98a7d795cbe81
Naveen Mathews <naveenrenji55@gmail.com>
2024-02-28 00:08:44 -0500

    new llama

--- Backend/chatbot.py
+++ Backend/chatbot.py
@@ -1,33 +1,31 @@
-# from stageOne.get_answer import get_answer
-# from stageOne.getcontext import get_answer
-# from stageOne.getcontext import load_data
-# from stageTwo.dataloading import get_SSE_results
-# from stageTwo.dataloading import load_model_and_data
-# from stageOne.get_context import init_data
-# from stageTwo.SSEmodel import get_SSE_results
-from stageThree.responseGenfinetuned import response_generator
+from stageOne.getcontext import get_answer
+from stageOne.getcontext import load_data
+from stageTwo.dataloading import get_SSE_results
+from stageTwo.dataloading import load_model_and_data
+from stageTwo.SSEmodel import get_SSE_results
+from stageThree.responseGenerator import response_generator
 from time import time
 
 
 def get_bot_response(query):
     t=time()
-    # model, question_embeddings, questions, df = load_data()
-    # context = get_answer(query, model, question_embeddings, questions, df)
-    #If the response was '', there was no match at stage 1, hence -> stage 2 component
-    # if context == 'not found':
-    #     sseModel, corpus_sentences, corpus_embeddings = load_model_and_data()
-    #     context = get_SSE_results(query, sseModel, corpus_sentences, corpus_embeddings)  # Implement your model function here
-    #     print(context) # for debugging only
-    #     pass  # Placeholder for stage 2 component
+    model, question_embeddings, questions, df = load_data()
+    context = get_answer(query, model, question_embeddings, questions, df)
+    # If the response was '', there was no match at stage 1, hence -> stage 2 component
+    if context == 'not found':
+        sseModel, corpus_sentences, corpus_embeddings = load_model_and_data()
+        context = get_SSE_results(query, sseModel, corpus_sentences, corpus_embeddings)  # Implement your model function here
+        print(context) # for debugging only
+        pass  # Placeholder for stage 2 component
 
     # Now we pass the data to the stage 3 component. 
-    # if context == 'not found':
-    #     response = "Sorry, I do not have the answer to that, please ask me another question."
-    # else: 
-    #     response = response_generator(query, context)
+    if context == 'not found':
+        response = "Sorry, I do not have the answer to that, please ask me another question."
+    else: 
+        response = response_generator(query, context)
     #     #response = "Temporary response data -- " + context
-
-    response = response_generator(query, "My name is naveen")
+    # print("started response generation")
+    # response = response_generator(query, "My name is naveen")
     print('Total time:', round(time() - t, 4), 'seconds')
     return response
 
--- Backend/stageThree/responseGenerator.py
+++ Backend/stageThree/responseGenerator.py
@@ -1,22 +1,18 @@
-# Conduct query with retrieval of sources
-query = "tell me about myself"
-source_information ="My name is Naveen I am a boy forom bahrain and i love to play agmes with all my fiendds"
-combined_information = f"Query: {query}\nContinue to answer the query by using the context: \n{source_information}."
-print(combined_information)
-auth_token = "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"
-# Load model directly
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf", use_auth_token=auth_token)
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf", use_auth_token=auth_token, device_map="auto")
-# tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b-it")
+def response_generator(question, context):
+    auth_token = "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"
+    tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf", use_auth_token=auth_token)
+    model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf", use_auth_token=auth_token, device_map="auto")
+    # tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b-it")
+    combined_information = f"Query: {question}\nContinue to answer the query by using the context: \n{context}."
 
-# # CPU Enabled uncomment below 👇🏽
-# model = AutoModelForCausalLM.from_pretrained("google/gemma-2b-it")
-# GPU Enabled use below 👇🏽
-# model = AutoModelForCausalLM.from_pretrained("google/gemma-2b-it", use_auth_token=auth_token, device_map="auto")
+    # # CPU Enabled uncomment below 👇🏽
+    # model = AutoModelForCausalLM.from_pretrained("google/gemma-2b-it")
+    # GPU Enabled use below 👇🏽
+    # model = AutoModelForCausalLM.from_pretrained("google/gemma-2b-it", use_auth_token=auth_token, device_map="auto")
 
-# Moving tensors to GPU
-input_ids = tokenizer(combined_information, return_tensors="pt").to("cuda")
-response = model.generate(**input_ids, max_new_tokens=500)
-print(tokenizer.decode(response[0]))
+    # Moving tensors to GPU
+    input_ids = tokenizer(combined_information, return_tensors="pt").to("cuda")
+    response = model.generate(**input_ids, max_new_tokens=500)
+    return (tokenizer.decode(response[0]))
