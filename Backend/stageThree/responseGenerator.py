66537c0b0040f232499b8ddd9e1482ef5864e31a
Naveen Mathews <naveenrenji55@gmail.com>
2024-02-27 23:37:49 -0500

    gemma

--- Backend/stageThree/responseGenerator.py
+++ Backend/stageThree/responseGenerator.py
@@ -1,10 +1,17 @@
-# Use a pipeline as a high-level helper
-from transformers import pipeline
+# Conduct query with retrieval of sources
+query = "tell me about myself"
+source_information ="My name is Naveen I am a boy forom bahrain and i love to play agmes with all my fiendds"
+combined_information = f"Query: {query}\nContinue to answer the query by using the context: \n{source_information}."
+print(combined_information)
+from transformers import AutoTokenizer, AutoModelForCausalLM
+tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b-it")
 
+# CPU Enabled uncomment below 👇🏽
+# model = AutoModelForCausalLM.from_pretrained("google/gemma-2b-it")
+# GPU Enabled use below 👇🏽
+model = AutoModelForCausalLM.from_pretrained("google/gemma-2b-it", device_map="auto")
 
-def response_generator(question, context):
-    pipe = pipeline("question-answering", model="google/gemma-2b")
-    response = pipe(question=question, context=context)
-    return response['answer']
-
-res = print(response_generator("tell me about myself", "My name is Naveen I am a boy forom bahrain and i love to play agmes with all my fiendds"))
+# Moving tensors to GPU
+input_ids = tokenizer(combined_information, return_tensors="pt").to("cuda")
+response = model.generate(**input_ids, max_new_tokens=500)
+print(tokenizer.decode(response[0]))
