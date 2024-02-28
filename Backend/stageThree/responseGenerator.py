2859a9214634abcbb13648f1d79be71d1c3ebd29
Naveen Mathews <naveenrenji55@gmail.com>
2024-02-28 18:33:56 -0500

    update

--- Backend/stageThree/responseGenerator.py
+++ Backend/stageThree/responseGenerator.py
@@ -16,3 +16,16 @@ def response_generator(question, context):
     input_ids = tokenizer(combined_information, return_tensors="pt").to("cuda")
     response = model.generate(**input_ids, max_new_tokens=500)
     return (tokenizer.decode(response[0], num_return_sequences=1, skip_special_tokens=True))
+
+
+def main():
+    print("inside main")
+    while True:
+        userinput = input("User: ")
+        if userinput.lower() == "stop":
+            break
+        else:
+            response = response_generator(userinput, "My name is Naveen, I am from Bahrain, I love shopping and playing football. My hobbies include reading and dancing.")
+            print(f"Assistant: {response}")
+
+main()
