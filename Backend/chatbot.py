19c9746c55fdc06ccb1545720dfdaa67c7c7c029
Naveen Mathews <naveenrenji55@gmail.com>
2024-03-01 19:12:16 -0500

    docker fix

--- Backend/chatbot.py
+++ Backend/chatbot.py
@@ -1,20 +1,35 @@
-from stageOne.getcontext import get_answer
-from stageOne.getcontext import load_data
-from stageTwo.dataloading import get_SSE_results
-from stageTwo.dataloading import load_model_and_data
-from stageTwo.SSEmodel import get_SSE_results
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning, module="torch._utils")
+
+from stageOne.getcontext import get_answer, load_data
+from stageTwo.dataloading import get_SSE_results, load_model_and_data
 from stageThree.responseGenerator import response_generator
 from time import time
 
 
+# Initialize global variables to None
+stageOneModel = None
+question_embeddings = None
+questions = None
+df = None
+stageTwoModel = None
+corpus_sentences = None
+corpus_embeddings = None
+
 def get_bot_response(query):
+    print("started")
     t=time()
-    model, question_embeddings, questions, df = load_data()
-    context = get_answer(query, model, question_embeddings, questions, df)
+    # Load data for stage one only if it hasn't been loaded before
+    if stageOneModel is None or question_embeddings is None or questions is None or df is None:
+        stageOneModel, question_embeddings, questions, df = load_data()
+    context = get_answer(query, stageOneModel, question_embeddings, questions, df)
+    print("stage 1 done")
+
     # If the response was '', there was no match at stage 1, hence -> stage 2 component
     if context == 'not found':
-        sseModel, corpus_sentences, corpus_embeddings = load_model_and_data()
-        context = get_SSE_results(query, sseModel, corpus_sentences, corpus_embeddings)  # Implement your model function here
+        if stageTwoModel is None or corpus_sentences is None or corpus_embeddings is None:
+            stageTwoModel, corpus_sentences, corpus_embeddings = load_model_and_data()
+        context = get_SSE_results(query, stageTwoModel, corpus_sentences, corpus_embeddings)  # Implement your model function here
         print(context) # for debugging only
         pass  # Placeholder for stage 2 component
 
@@ -23,9 +38,9 @@ def get_bot_response(query):
         response = "Sorry, I do not have the answer to that, please ask me another question."
     else: 
         response = response_generator(query, context)
-    #     #response = "Temporary response data -- " + context
-    # print("started response generation")
-    # response = response_generator(query, "My name is naveen")
+        # response = "Temporary response data -- " + context
+
+    print("started response generation")
     print('Total time:', round(time() - t, 4), 'seconds')
     return response
 
