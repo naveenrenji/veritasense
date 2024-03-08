67a6e782bc0d446bd17222c8148761e8b41ec5ac
Naveen Mathews <naveenrenji55@gmail.com>
2024-03-07 19:09:40 -0500

    trial

--- Backend/chatbot.py
+++ Backend/chatbot.py
@@ -21,25 +21,26 @@ from time import time
 def get_bot_response(query):
     print("started")
     t=time()
-    # # Load data for stage one only if it hasn't been loaded before
-    # if loadedStageOneModel is False:
-    #     stageOneModel, question_embeddings, questions, df = load_data()
-    #     loadedStageOneModel = True
-    stageOneModel, question_embeddings, questions, df = load_data()
-    context = get_answer(query, stageOneModel, question_embeddings, questions, df)
-    print("stage 1 done")
+    # # # Load data for stage one only if it hasn't been loaded before
+    # # if loadedStageOneModel is False:
+    # #     stageOneModel, question_embeddings, questions, df = load_data()
+    # #     loadedStageOneModel = True
+    # stageOneModel, question_embeddings, questions, df = load_data()
+    # context = get_answer(query, stageOneModel, question_embeddings, questions, df)
+    # print("stage 1 done")
 
-    # If the response was '', there was no match at stage 1, hence -> stage 2 component
-    if context == 'not found':
-        # if loadedStageTwoModel is False:
-        #     stageTwoModel, corpus_sentences, corpus_embeddings = load_model_and_data()
-        #     loadedStageTwoModel = True
-        stageTwoModel, corpus_sentences, corpus_embeddings = load_model_and_data()
-        context = get_SSE_results(query, stageTwoModel, corpus_sentences, corpus_embeddings)  # Implement your model function here
-        print(context) # for debugging only
-        pass  # Placeholder for stage 2 component
+    # # If the response was '', there was no match at stage 1, hence -> stage 2 component
+    # if context == 'not found':
+    #     # if loadedStageTwoModel is False:
+    #     #     stageTwoModel, corpus_sentences, corpus_embeddings = load_model_and_data()
+    #     #     loadedStageTwoModel = True
+    #     stageTwoModel, corpus_sentences, corpus_embeddings = load_model_and_data()
+    #     context = get_SSE_results(query, stageTwoModel, corpus_sentences, corpus_embeddings)  # Implement your model function here
+    #     print(context) # for debugging only
+    #     pass  # Placeholder for stage 2 component
 
     # Now we pass the data to the stage 3 component. 
+    context = "python3.0 is the version we use in class"
     if context == 'not found':
         response = "Sorry, I do not have the answer to that, please ask me another question."
     else: 
