a93c594442614421c9823051ff7f889c943d0667
Naveen Mathews <naveenrenji55@gmail.com>
2024-03-07 19:13:19 -0500

    update

--- Backend/stageThree/new.py
+++ Backend/stageThree/new.py
@@ -81,7 +81,7 @@ generate_text = transformers.pipeline(
 
 
 def response_generator(question, context):
-    res = generate_text(f"Answer this Question based on the context, you are playing the role of a computer science professor chatbot: {question}\nThis is the context to use - Context: {context}. now respond-")
+    res = generate_text(f"Answer this Question based on the context, you are playing the role of a computer science professor chatbot: {question}\nThis is the context to use - Context: {context}. now respond based on the context -")
     generated_text = res[0]["generated_text"]
      # Find the index of "now respond-" and slice the text from that point forward
     respond_index = generated_text.find("now respond-")
