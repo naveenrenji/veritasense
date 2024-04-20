02183bc47a82083dfa65f128ff29a40043f31e7b
Naveen Mathews <naveenrenji55@gmail.com>
2024-04-20 14:30:02 -0400

    gpt22.py

--- Backend/stageThree/gpt22.py
+++ Backend/stageThree/gpt22.py
@@ -1,22 +1,17 @@
-from transformers import GPTNeoXForCausalLM, GPTNeoXTokenizerFast
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+device = "cuda" # the device to load the model onto
 
-# model = GPTNeoXForCausalLM.from_pretrained("EleutherAI/gpt-neox-20b")
-model = GPTNeoXForCausalLM.from_pretrained("EleutherAI/gpt-neox-20b").half().cuda()
-tokenizer = GPTNeoXTokenizerFast.from_pretrained("EleutherAI/gpt-neox-20b")
+model = AutoModelForCausalLM.from_pretrained("gpt2", torch_dtype=torch.float16, attn_implementation="flash_attention_2")
+tokenizer = AutoTokenizer.from_pretrained("gpt2")
 
 question = "Explain Tokenisation in NLP?"
 context = """Tokenization is the process of replacing sensitive data with a surrogate value, or token, that is non-sensitive and randomized. Tokens are unique identifiers that retain all the important information about the data without compromising its security. For example, a credit card number can be replaced with a token, which is a string of randomized characters. 
 Tokenization can also refer to the process of breaking down a sequence of text into smaller parts, known as tokens. These tokens can be as small as characters or as long as words. For example, tokenizing the sentence “I love ice cream” would result in three tokens: “I,” “love,” and “ice cream”. This process is fundamental in natural language processing and text analysis tasks. """
 prompt = f"Answer this Question based on the context, you are playing the role of a computer science professor chatbot: {question}\nThis is the context to use - Context: {context}. now respond based on the context -"
 
-input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+model_inputs = tokenizer([prompt], return_tensors="pt").to(device)
+model.to(device)
 
-gen_tokens = model.generate(
-    input_ids,
-    do_sample=True,
-    temperature=0.9,
-    max_length=100,
-)
-gen_text = tokenizer.batch_decode(gen_tokens)[0]
-
-print(gen_text)
+generated_ids = model.generate(**model_inputs, max_new_tokens=100, do_sample=True)
+tokenizer.batch_decode(generated_ids)[0]
