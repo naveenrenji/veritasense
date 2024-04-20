e2fe31ba700635134cc653f99cffd24e270b539c
Naveen Mathews <naveenrenji55@gmail.com>
2024-04-20 14:03:07 -0400

    fix

--- Backend/stageThree/llama3.py
+++ Backend/stageThree/llama3.py
@@ -10,7 +10,7 @@ access_token = "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"
 
 
 model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", load_in_4bit=True,  use_auth_token=access_token)
-tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True, use_auth_token=access_token)
+tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True,return_token_type_ids=False, use_auth_token=access_token)
 model_inputs = tokenizer(prompt, return_tensors="pt").to("cuda:0")
 
 output = model.generate(**model_inputs)
