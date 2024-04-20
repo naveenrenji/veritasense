52b5939ec446177251e8417e3be57ff7b0c9ff7f
Naveen Mathews <naveenrenji55@gmail.com>
2024-04-20 13:59:24 -0400

    updtellma

--- Backend/stageThree/llama3.py
+++ Backend/stageThree/llama3.py
@@ -1,8 +1,5 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
-import torch
-
-
-model_name = "meta-llama/Meta-Llama-3-8B"
+model_name = "meta-llama/Llama-2-7b-chat-hf"
 question = "Explain Tokenisation in NLP?"
 context = """Tokenization is the process of replacing sensitive data with a surrogate value, or token, that is non-sensitive and randomized. Tokens are unique identifiers that retain all the important information about the data without compromising its security. For example, a credit card number can be replaced with a token, which is a string of randomized characters. 
 Tokenization can also refer to the process of breaking down a sequence of text into smaller parts, known as tokens. These tokens can be as small as characters or as long as words. For example, tokenizing the sentence “I love ice cream” would result in three tokens: “I,” “love,” and “ice cream”. This process is fundamental in natural language processing and text analysis tasks. """
@@ -10,10 +7,10 @@ prompt = f"Answer this Question based on the context, you are playing the role o
 
 access_token = "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"
 
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", load_in_8bit=True, use_auth_token=access_token)
-tokenizer = AutoTokenizer.from_pretrained(model_name, max_new_tokens=512, use_fast=True, use_auth_token=access_token)
+
+model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", load_in_4bit=True,  use_auth_token=access_token)
+tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True, use_auth_token=access_token)
 model_inputs = tokenizer(prompt, return_tensors="pt").to("cuda:0")
 
 output = model.generate(**model_inputs)
