b799c9f7584c34455a5c60688c4aca1d58850ed4
Naveen Mathews <naveenrenji55@gmail.com>
2024-04-20 14:24:05 -0400

    gema

--- Backend/stageThree/gemma.py
+++ Backend/stageThree/gemma.py
@@ -1,20 +1,15 @@
-# pip install accelerate
-from transformers import AutoTokenizer, AutoModelForCausalLM
-import torch
+from transformers import AutoTokenizer, GemmaForCausalLM
 
-tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b-it")
-model = AutoModelForCausalLM.from_pretrained(
-    "google/gemma-2b-it",
-    device_map="auto",
-    torch_dtype=torch.bfloat16
-)
+model = GemmaForCausalLM.from_pretrained("google/gemma-7b")
+tokenizer = AutoTokenizer.from_pretrained("google/gemma-7b")
 
 question = "Explain Tokenisation in NLP?"
 context = """Tokenization is the process of replacing sensitive data with a surrogate value, or token, that is non-sensitive and randomized. Tokens are unique identifiers that retain all the important information about the data without compromising its security. For example, a credit card number can be replaced with a token, which is a string of randomized characters. 
 Tokenization can also refer to the process of breaking down a sequence of text into smaller parts, known as tokens. These tokens can be as small as characters or as long as words. For example, tokenizing the sentence “I love ice cream” would result in three tokens: “I,” “love,” and “ice cream”. This process is fundamental in natural language processing and text analysis tasks. """
 prompt = f"Answer this Question based on the context, you are playing the role of a computer science professor chatbot: {question}\nThis is the context to use - Context: {context}. now respond based on the context -"
+inputs = tokenizer(prompt, return_tensors="pt")
 
-input_ids = tokenizer(prompt, return_tensors="pt").to("cuda")
-
-outputs = model.generate(**input_ids)
-print(tokenizer.decode(outputs[0]))
+# Generate
+generate_ids = model.generate(inputs.input_ids, max_length=30)
+output = tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+print(output)
