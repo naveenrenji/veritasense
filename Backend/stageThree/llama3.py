809b0cdcf4368e5c12b58f2b627e7e1a4b2afee5
Naveen Mathews <naveenrenji55@gmail.com>
2024-04-20 13:52:44 -0400

    16 bit

--- Backend/stageThree/llama3.py
+++ Backend/stageThree/llama3.py
@@ -12,7 +12,7 @@ access_token = "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"
 
 # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", load_in_4bit=True,  use_auth_token=access_token)
+model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", load_in_16bit=True,  use_auth_token=access_token)
 tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True, use_auth_token=access_token)
 model_inputs = tokenizer(prompt, return_tensors="pt").to("cuda:0")
 
