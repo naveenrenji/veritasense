301ab526024fbbc02b10904a416ee1b44bae51a4
Naveen Mathews <naveenrenji55@gmail.com>
2024-04-20 14:20:46 -0400

    gemma update

--- Backend/stageThree/gemma.py
+++ Backend/stageThree/gemma.py
@@ -1,22 +1,20 @@
+# pip install accelerate
 from transformers import AutoTokenizer, AutoModelForCausalLM
-import transformers
 import torch
 
-model_id = "gg-hf/gemma-2b-it"
-dtype = torch.bfloat16
-
-tokenizer = AutoTokenizer.from_pretrained(model_id)
+tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b-it")
 model = AutoModelForCausalLM.from_pretrained(
-    model_id,
-    device_map="cuda",
-    torch_dtype=dtype,
+    "google/gemma-2b-it",
+    device_map="auto",
+    torch_dtype=torch.bfloat16
 )
 
-chat = [
-    { "role": "user", "content": "Write a hello world program" },
-]
-prompt = tokenizer.apply_chat_template(chat, tokenize=False, add_generation_prompt=True)
+question = "Explain Tokenisation in NLP?"
+context = """Tokenization is the process of replacing sensitive data with a surrogate value, or token, that is non-sensitive and randomized. Tokens are unique identifiers that retain all the important information about the data without compromising its security. For example, a credit card number can be replaced with a token, which is a string of randomized characters. 
+Tokenization can also refer to the process of breaking down a sequence of text into smaller parts, known as tokens. These tokens can be as small as characters or as long as words. For example, tokenizing the sentence “I love ice cream” would result in three tokens: “I,” “love,” and “ice cream”. This process is fundamental in natural language processing and text analysis tasks. """
+prompt = f"Answer this Question based on the context, you are playing the role of a computer science professor chatbot: {question}\nThis is the context to use - Context: {context}. now respond based on the context -"
 
+input_ids = tokenizer(prompt, return_tensors="pt").to("cuda")
 
-inputs = tokenizer.encode(prompt, add_special_tokens=False, return_tensors="pt")
-outputs = model.generate(input_ids=inputs.to(model.device), max_new_tokens=350)
+outputs = model.generate(**input_ids)
+print(tokenizer.decode(outputs[0]))
