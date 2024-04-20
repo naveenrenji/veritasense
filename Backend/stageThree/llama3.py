445f2e399232cac29d3980527ef00f64f2d61769
Naveen Mathews <naveenrenji55@gmail.com>
2024-04-20 12:39:52 -0400

    upodate

--- Backend/stageThree/llama3.py
+++ Backend/stageThree/llama3.py
@@ -1,6 +1,10 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 model_name = "meta-llama/Meta-Llama-3-8B"
-prompt = "Tell me about gravity"
+question = "Explain Tokenisation in NLP?"
+context = """Tokenization is the process of replacing sensitive data with a surrogate value, or token, that is non-sensitive and randomized. Tokens are unique identifiers that retain all the important information about the data without compromising its security. For example, a credit card number can be replaced with a token, which is a string of randomized characters. 
+Tokenization can also refer to the process of breaking down a sequence of text into smaller parts, known as tokens. These tokens can be as small as characters or as long as words. For example, tokenizing the sentence “I love ice cream” would result in three tokens: “I,” “love,” and “ice cream”. This process is fundamental in natural language processing and text analysis tasks. """
+prompt = f"Answer this Question based on the context, you are playing the role of a computer science professor chatbot: {question}\nThis is the context to use - Context: {context}. now respond based on the context -"
+
 access_token = "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"
 
 
