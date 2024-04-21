db82836a4fe2c68de0fe10dbb72b2ef321de576a
Naveen Mathews <naveenrenji55@gmail.com>
2024-04-21 16:30:22 -0400

    fix

--- Backend/chatbot.py
+++ Backend/chatbot.py
@@ -4,7 +4,7 @@ warnings.filterwarnings("ignore", category=UserWarning, module="torch._utils")
 from stageOne.getcontext import get_answer, load_data
 from stageTwo.getResponse import get_answer2, load_data2
 # from stageTwo.dataloading import get_SSE_results, load_model_and_data
-from stageThree.gptneo import response_generator
+from stageThree.gptneo import generate_response
 from time import time
 
 
@@ -48,7 +48,7 @@ def get_bot_response(query):
     if context == 'not found':
         response = "Hello, please ask me a question related to Python Programming."
     else: 
-        response = response_generator(query, context)
+        response = generate_response(query, context)
         # response = "Temporary response data -- " + context
 
     print("started response generation")
@@ -60,6 +60,6 @@ def get_bot_response(query):
 #     return "mock"
 
 
-print(get_bot_response("what is object oriented programming?"))
+# print(get_bot_response("what is object oriented programming?"))
 
-print(get_bot_response("what version of python are we using in class?"))
+# print(get_bot_response("what version of python are we using in class?"))
--- Backend/stageThree/gptneo.py
+++ Backend/stageThree/gptneo.py
@@ -50,18 +50,19 @@ def generate_response(question, context):
 
     # Print only the answer
     print(answer)
+    return answer
 
-# Example question and context
-question = "What is the role of AI in healthcare?"
-context = """Artificial Intelligence in healthcare is used for diagnoses, treatment recommendations, and patient monitoring. AI can analyze complex medical data much faster than a human doctor and predict diseases early."""
+# # Example question and context
+# question = "What is the role of AI in healthcare?"
+# context = """Artificial Intelligence in healthcare is used for diagnoses, treatment recommendations, and patient monitoring. AI can analyze complex medical data much faster than a human doctor and predict diseases early."""
 
-# Generate and print the response
-generate_response(question, context)
+# # Generate and print the response
+# generate_response(question, context)
 
-print ("next! ")
+# print ("next! ")
 
-question = "Explain Tokenisation in NLP?"
-context = """Tokenization is the process of replacing sensitive data with a surrogate value, or token, that is non-sensitive and randomized. Tokens are unique identifiers that retain all the important information about the data without compromising its security. For example, a credit card number can be replaced with a token, which is a string of randomized characters. 
-Tokenization can also refer to the process of breaking down a sequence of text into smaller parts, known as tokens. These tokens can be as small as characters or as long as words. For example, tokenizing the sentence “I love ice cream” would result in three tokens: “I,” “love,” and “ice cream”. This process is fundamental in natural language processing and text analysis tasks. """
+# question = "Explain Tokenisation in NLP?"
+# context = """Tokenization is the process of replacing sensitive data with a surrogate value, or token, that is non-sensitive and randomized. Tokens are unique identifiers that retain all the important information about the data without compromising its security. For example, a credit card number can be replaced with a token, which is a string of randomized characters. 
+# Tokenization can also refer to the process of breaking down a sequence of text into smaller parts, known as tokens. These tokens can be as small as characters or as long as words. For example, tokenizing the sentence “I love ice cream” would result in three tokens: “I,” “love,” and “ice cream”. This process is fundamental in natural language processing and text analysis tasks. """
 
-generate_response(question, context)
+# generate_response(question, context)
